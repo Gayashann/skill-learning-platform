66d62ee7befd739c2a7134306eafaea2ec4a8198
Gayashan kavindu <168892020+Gayashann@users.noreply.github.com>
2025-05-06 12:16:36 +0530

    comment

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     hidden: { opacity: 0, y: 20 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
   };
-  //hello
+  //good
 
   return (
     <motion.footer
