f7286b1ff030f3b182eec7acb384d7c63d0db883
Sahan MAtheesha <168654045+SahanMatheesha@users.noreply.github.com>
2025-05-14 19:37:33 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { SunIcon, MoonIcon, PencilIcon, TrashIcon, HandThumbUpIcon, ChatBubbleLe
 import { FaWhatsapp, FaFacebook } from "react-icons/fa";
 
 //HOME JSX
-//home page jsx
+
 export default function Home() {
   const {
     user,
