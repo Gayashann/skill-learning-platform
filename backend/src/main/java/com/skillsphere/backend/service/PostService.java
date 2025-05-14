c6c58cc9452a762635ef576175ed2e5e9399a03e
Sahan MAtheesha <168654045+SahanMatheesha@users.noreply.github.com>
2025-05-14 19:36:01 +0530

    Update PostService.java

--- backend/src/main/java/com/skillsphere/backend/service/PostService.java
+++ backend/src/main/java/com/skillsphere/backend/service/PostService.java
@@ -18,7 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+    //import org.springframework.web.multipart.MultipartFile;
 @Service
 public class PostService {
     @Autowired
